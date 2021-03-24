ecdfa09f868ed007089766a85544bd0ed8cd8d7b
Phil <60888215+phileeep@users.noreply.github.com>
2021-03-24 08:58:12 +0100

    added user input for interactivity

--- directory.rb
+++ directory.rb
@@ -29,6 +29,24 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Pleae enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do 
+    students << {name: name, cohort: :march}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
