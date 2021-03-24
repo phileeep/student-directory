f2d3673569783a6b8de062e1322e8dbd047525d5
Phil <60888215+phileeep@users.noreply.github.com>
2021-03-24 08:43:22 +0100

    added hash values to improve readability

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #putting all our students now into an array
 students = [
-  "Dr Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr Hannibal Lecter",  cohort: :march},
+  {name: "Darth Vader", cohort: :march},
+  {name: "Nurse Ratched", cohort: :march},
+  {name: "Michael Corleone", cohort: :march},
+  {name: "Alex DeLarge", cohort: :march},
+  {name: "The Wicked Witch of the West", cohort: :march},
+  {name: "Terminator", cohort: :march},
+  {name: "Freddy Krueger", cohort: :march},
+  {name: "The Joker", cohort: :march},
+  {name: "Joffrey Baratheon", cohort: :march},
+  {name: "Norman Bates", cohort: :march}
 ]
 
 #and then print them 
@@ -19,14 +19,14 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 print_header
