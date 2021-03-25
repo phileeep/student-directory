40d398f71dd3e08142e5503bec377a8cf3b4740d
Phil <60888215+phileeep@users.noreply.github.com>
2021-03-25 07:47:52 +0100

    made the cohort value not hard coded and added a choice clause

--- questions/directory_questions.rb
+++ questions/directory_questions.rb
@@ -17,25 +17,46 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-def input_students
-  puts "Pleae enter the names of the students"
+def input_students(name = "John")
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
   students = []
 
   name = gets.chomp.capitalize
 
+  puts "Your name is #{name} is that correct? Please type Y / N"
+  choice = gets.chomp
+
+  while choice == 'N' || choice.empty?
+    puts "Please enter the names of the students" 
+    name = gets.chomp.capitalize
+    puts "Your name is #{name} is that correct? Please type Y / N"
+    choice = gets.chomp
+  end
+
   while !name.empty? do 
+    puts "Which cohort are they in?"
+    cohort = gets.chomp.to_sym
+
+    while cohort.empty?
+      puts "Which cohort are they in?"
+      cohort = gets.chomp.to_sym
+      if cohort.empty?
+        cohort = "march"
+      end
+
     puts "Please enter their Hobbies"
     hobbies = gets.chomp.capitalize
   
     puts "Please enter their Date of Birth"
     dateofbirth = gets.chomp.capitalize
-    students << {name: name, cohort: 'March', dateofbirth: dateofbirth, hobbies: hobbies}
+    students << {name: name, cohort: cohort, dateofbirth: dateofbirth, hobbies: hobbies}
     puts "Now we have #{students.count} students"
     puts "Pleae enter the names of the students"
     name = gets.chomp
   end
+end
 
   students
 end
