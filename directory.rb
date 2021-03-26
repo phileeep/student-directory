0b80805e2aa8ccb7b8c7dfc9768ba5fa7cdbb9d5
Phil <60888215+phileeep@users.noreply.github.com>
2021-03-26 17:49:04 +0100

    added ability to load csv files

--- directory.rb
+++ directory.rb
@@ -94,6 +94,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else 
@@ -105,6 +107,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" 
 end
 	
@@ -124,10 +127,19 @@ end
 def save_students
   file = File.open("students.csv", "w")
 
-  @students.each do |students|
-    student_Data =[student[:name], student[:cohort]]
+  @students.each do |student|
+    student_data =[student[:name], student[:cohort]]
     csv_line = student_data.join(',')
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
